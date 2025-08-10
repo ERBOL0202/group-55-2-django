ddb7555194579fd65ce5831dcdac40bb55ee7022
Erbol <kul8287@gmail.com>
2025-08-10 18:33:10 +0300

    hw2

---
+++ posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from .models import Post
+
+
+def home(request):
+    return render(request, "home.html")
+
+def test_view(request):
+    return render(request, "test.html")
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, "post_list.html", context={"posts": posts})
